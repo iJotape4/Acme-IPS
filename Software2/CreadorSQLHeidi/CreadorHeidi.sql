dc17fd4225db2ea9b07124878d8fcc65b51d8813
Juan Pablo <jcamacho249@unab.edu.co>
2020-11-10 23:22:46 -0500

    Administración sesiones

---
+++ Software2/CreadorSQLHeidi/CreadorHeidi.sql
@@ -0,0 +1,8 @@
+	
+    INSERT `gestiondecitas_paciente` (`Id`, `PrimerNombre`, `SegundoNombre`, `PrimerApellido`, `SegundoApellido`, `Usuario`, `Contraseña`, `DocumentoId`, `TipoDocumento`, `Edad`, `CorreoElectronico`, `EPSP`,`TipoUsuario`,`Telefono`, `Whatsapp`,`Hipertension`,`Diabetes`, `Cardiacos`,`Ciudad`,`Barrio`) VALUES
+	(1, 'Jhoan', 'Jesus', 'Ortiz', 'Sandoval', 'jortiz', '132', 1005543307, 'CC', 20, 'jortiz519@unab.edu.co', 'Nueva EPS','Paciente',311872922,311872922,0,0,0,'Bucaramanga','Ruitoque'),
+	(2, 'Javier', 'Yesid', 'Parra', 'Carrillo', 'jparra', '123', 1004835678, 'CC', 19, 'jparra@unab.edu.co', 'Nueva EPS','Paciente',3163355805,311872922,0,0,0,'Giron','Villamil');
+
+	 INSERT `gestiondecitas_secretaria` (`Id`, `PrimerNombre`, `SegundoNombre`, `PrimerApellido`, `SegundoApellido`, `Usuario`, `Contraseña`, `DocumentoId`, `TipoDocumento`, `Edad`, `CorreoElectronico`,`TipoUsuario`, `Whatsapp`) VALUES
+	(1, 'Maria', 'Angelica', 'Mosquera', 'Sandoval', 'mmosquera', '132', 1005543307, 'CC', 22, 'mmosquera@unab.edu.co','Secretaria',3118826225),
+	(2, 'Angie', 'Valentina', 'Ortiz', 'Carrillo', 'aortiz', '123', 1004835678, 'CC', 21, 'aortiz@unab.edu.co','Secretaria',3125423633);
