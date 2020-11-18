05dde858fa550065a2326b4b0ce5ac647f5b4cec
Javer Parra <jparra305@unab.edu.co>
2020-11-18 00:42:58 -0500

    Ojala esta si

--- Software2/settings.py
+++ Software2/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 # Allow all host hosts/domain names for this site
 ALLOWED_HOSTS = ['*']
-DATABASES = { 'default' : dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config(default='postgresql')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
